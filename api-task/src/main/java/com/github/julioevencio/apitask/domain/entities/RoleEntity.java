a50716b0c92226ab3a8108c088879b9df433fd63
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-04-16 16:36:39 -0300

    Added ORM

---
+++ api-task/src/main/java/com/github/julioevencio/apitask/domain/entities/RoleEntity.java
@@ -0,0 +1,75 @@
+package com.github.julioevencio.apitask.domain.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class RoleEntity implements Serializable, GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 5)
+	private String name;
+
+	@Override
+	public String getAuthority() {
+		return name;
+	}
+
+	public RoleEntity() {
+		// Code
+	}
+
+	public RoleEntity(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RoleEntity other = (RoleEntity) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+}
