2f753a12634a3b76a7fa855fea6058c0b726e7b4
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-04-17 18:26:52 -0300

    Update UserRequestDTO

--- api-task/src/main/java/com/github/julioevencio/apitask/dto/user/UserRequestDTO.java
+++ api-task/src/main/java/com/github/julioevencio/apitask/dto/user/UserRequestDTO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRequestDTO implements Serializable {
@@ -14,7 +15,7 @@ public class UserRequestDTO implements Serializable {
 	@Length(max = 20, min = 1, message = "The username must be between 1 and 20 characters long")
 	private String username;
 
-	@NotBlank(message = "Invalid e-mail")
+	@Email(message = "Invalid e-mail")
 	@Length(max = 100, min = 1, message = "The e-mail must be between 1 and 100 characters long")
 	private String email;
 
@@ -24,7 +25,7 @@ public class UserRequestDTO implements Serializable {
 
 	public UserRequestDTO(
 			@NotBlank(message = "Invalid username") @Length(max = 20, min = 1, message = "The username must be between 1 and 20 characters long") String username,
-			@NotBlank(message = "Invalid e-mail") @Length(max = 100, min = 1, message = "The e-mail must be between 1 and 100 characters long") String email,
+			@Email(message = "Invalid e-mail") @Length(max = 100, min = 1, message = "The e-mail must be between 1 and 100 characters long") String email,
 			@NotBlank(message = "Invalid password") @Length(max = 20, min = 1, message = "The password must be between 1 and 20 characters long") String password) {
 		this.username = username;
 		this.email = email;
