10fcde49580fcc0e7359b95dac585e9af88feca2
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-04-24 18:36:02 -0300

    Added resource delete for TaskController

--- api-task/src/main/java/com/github/julioevencio/apitask/controllers/TaskController.java
+++ api-task/src/main/java/com/github/julioevencio/apitask/controllers/TaskController.java
@@ -6,12 +6,13 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.julioevencio.apitask.dto.task.TaskRequestDTO;
@@ -286,6 +287,108 @@ public class TaskController {
 		response.addLink(new LinkUtilDTO("me", "/api/users/me"));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			security = @SecurityRequirement(name = "bearerAuth"),
+			summary = "Update a task for a user",
+			description = "Update a task for a user",
+			tags = {"Task"},
+			responses = {
+					@ApiResponse(
+							responseCode = "200",
+							description = "Task Updated",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = TaskResponseDTO.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "400",
+							description = "Bad request",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "401",
+							description = "Unauthorized",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							description = "Forbidden",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					)
+			}
+	)
+	public ResponseEntity<TaskResponseDTO> update(@PathVariable UUID id, @RequestBody @Valid TaskRequestDTO dto) {
+		TaskResponseDTO response = taskService.update(id, dto);
+
+		response.addLink(new LinkUtilDTO("self", "/api/tasks/{id}"));
+		response.addLink(new LinkUtilDTO("find by id", "/api/tasks/{id}"));
+		response.addLink(new LinkUtilDTO("find all", "/api/tasks"));
+		response.addLink(new LinkUtilDTO("find by title", "/api/tasks/title/{title}"));
+		response.addLink(new LinkUtilDTO("find by completed", "/api/tasks/completed/{completed}"));
+		response.addLink(new LinkUtilDTO("create", "/api/tasks"));
+		response.addLink(new LinkUtilDTO("delete", "/api/tasks/{id}"));
+		response.addLink(new LinkUtilDTO("me", "/api/users/me"));
+
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			security = @SecurityRequirement(name = "bearerAuth"),
+			summary = "Delete a task for a user",
+			description = "Delete a task for a user",
+			tags = {"Task"},
+			responses = {
+					@ApiResponse(
+							responseCode = "204",
+							description = "Task Deleted",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE
+							)
+					),
+					@ApiResponse(
+							responseCode = "400",
+							description = "Bad request",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "401",
+							description = "Unauthorized",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					),
+					@ApiResponse(
+							responseCode = "403",
+							description = "Forbidden",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = ApiTaskMessageError.class)
+							)
+					)
+			}
+	)
+	public ResponseEntity<Void> delete(@PathVariable UUID id) {
+		taskService.delete(id);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
